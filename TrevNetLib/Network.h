0f4a46d0cc2e7a9dffdf756d9fcaba553e80f8f2
WildBoar <trevorgeek@gmail.com>
2023-02-14 22:27:43 +1300

    Add the stub for testing out the neural network here

---
+++ TrevNetLib/Network.h
@@ -0,0 +1,32 @@
+//--------------------------------------------------
+// The main wrapper for the network functionality
+//
+// @author: Wild Boar
+//
+// @date: 2023-02-14
+//--------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include "NetworkUtils.h"
+#include "Generator/NumberGenerator.h"
+
+namespace NVL_AI
+{
+	class Network
+	{
+		private:
+			vector<Layer *> _network;
+		public:
+			Network(NumberGenerator * generator, const vector<int>& structure);
+			~Network();
+
+			void Evaluate(const vector<double>& inputs, vector<double>& outputs);
+			double Update(const vector<double>& inputs, const vector<double>& expectedOutputs, double learnRate);
+
+			inline vector<Layer *>& GetNetwork() { return _network; }
+	};
+}
---
+++ TrevNetTests/Tests/Network_Tests.cpp
@@ -0,0 +1,64 @@
+//--------------------------------------------------
+// Unit Tests for class Network
+//
+// @author: Wild Boar
+//
+// @date: 2023-02-14
+//--------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <TrevNetLib/Network.h>
+using namespace NVL_AI;
+
+//--------------------------------------------------
+// Test Methods
+//--------------------------------------------------
+
+/**
+ * @brief Make sure that the network has the expected structure
+ */
+TEST(Network_Test, confirm_network)
+{
+	FAIL() << "Not implemented";
+
+	// Setup
+
+	// Execute
+
+	// Confirm
+
+	// Teardown
+}
+
+/**
+ * @brief Confirm that the network is able to evaluate expressions
+ */
+TEST(Network_Test, evaluate)
+{
+	FAIL() << "Not implemented";
+
+	// Setup
+
+	// Execute
+
+	// Confirm
+
+	// Teardown
+}
+
+/**
+ * @brief Confirm that the network is capable of performing an update
+ */
+TEST(Network_Test, update)
+{
+	FAIL() << "Not implemented";
+
+	// Setup
+
+	// Execute
+
+	// Confirm
+
+	// Teardown
+}
